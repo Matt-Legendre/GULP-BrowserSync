96b70d2848a8cbc42337120f9752a04bd0f27120
Mathieu Legendre <mathieu.legendre.0485@gmail.com>
2020-12-02 18:51:35 +0100

    let us get started with a real app !!!

--- app/js/script.js
+++ app/js/script.js
@@ -1,3 +1,42 @@
+const {
+  src,
+  dest,
+  watch,
+  series
+} = require('gulp');
+const sass = require('gulp-sass');
+const postcss = require('gulp-postcss');
+const cssnano = require('cssnano');
+const terser = require('gulp-terser');
+const browsersync = require('browser-sync').create();
+
+
+
+// Sass Task
+function scssTask() {
+  return src('app/scss/style.scss', {
+      sourcemaps: true
+    })
+    .pipe(sass())
+    .pipe(postcss([cssnano()]))
+    .pipe(dest('dist', {
+      sourcemaps: '.'
+    }));
+}
+
+
+// JavaScript Task
+function jsTask() {
+  return src('app/js/script.js', {
+      sourcemaps: true
+    })
+    .pipe(terser())
+    .pipe(dest('dist', {
+      sourcemaps: '.'
+    }));
+}
+
+
 const testString = 'This is a test!';
 
 console.log(testString);
--- readme.md
+++ readme.md
@@ -4,3 +4,6 @@ Source code for the Browsersync + Gulp tutorial on Coder Coder.
 
 Check out the full tutorials on [YouTube](https://youtu.be/q0E1hbcj-NI) and on [my blog](https://coder-coder.com/quick-guide-to-browsersync-gulp-4/)!
 
+___
+
+This a **fork** by Mathieu Legendre --- thx to the original author for this learning opportunity :blush:
